848ccdfc8c88f44dd74af45c7271b20b50472bdf
uralenergo <uralenergo@mai;.ru>
2018-04-20 09:41:35 +0500

    new

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+(function($) {
+
+  new WOW().init();
+
+  jQuery(window).load(function() {
+    jQuery("#preloader").delay(100).fadeOut("slow");
+    jQuery("#load").delay(100).fadeOut("slow");
+  });
+
+
+  $(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+  });
+
+  $(function() {
+
+    $('.navbar-nav li a').on('click', function(event) {
+
+      if ($(this).is('a:not([href^="#"])') || $(this).attr("href") == '#') {
+        return;
+      }
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
+
+    $('.page-scroll a').on('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
+
+  });
+	
+	var navMain = $(".navbar-collapse");
+	navMain.on("click", "a:not([data-toggle])", null, function () {
+	   navMain.collapse('hide');
+	});
+
+})(jQuery);
